63c711f6b5d78db1379b8dd262f69c0b7963b51e
Minh Tran <128418954+tranhoangduyminh@users.noreply.github.com>
2024-08-26 07:08:46 +0700

    update

---
+++ project_sql/3_top_demanded_skills.sql
@@ -0,0 +1,20 @@
+/*
+Question: What are the most in-demand skills for business analyst?
+- Join job postings to inner join table similar to query 2
+- Identify top 5 in-demand skills for a business analyst
+- Focus on all job postings
+=> Retrieves the top 5 skills with the highest demand in the job market, providing insights to the most valuable skills for job seekers.
+*/
+
+SELECT
+  skills,
+  COUNT(skills_job_dim.job_id) AS demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+  job_title_short = 'Business Analyst' AND
+  job_work_from_home = true
+GROUP BY skills
+ORDER BY demand_count DESC
+LIMIT 5
